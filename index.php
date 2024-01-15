2331b500d459e05562de883be30ede1b31761760
Luiz Felipe Cordeiro <114431500+Felipex7z@users.noreply.github.com>
2024-01-15 20:55:48 -0300

    cmt

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastre-se</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="full">
+        <div class="form">
+            <form action="cad.php" method="POST">
+                <label for="nome">Nome: </label>
+                <input type="text" name="nome" id="nome" placeholder="Digite seu Nome">
+
+                <label for="sobrenome">Sobrenome: </label>
+                <input type="text" name="sobrenome" id="sobrenome" placeholder="Digite seu sobrenome">
+
+
+                <label for="sobrenome">E-Mail: </label>
+                <input type="text" name="email" id="email" placeholder="Seu E-mail">
+
+
+                <label for="sobrenome">Senha: </label>
+                <input type="text" name="senha" id="senha" placeholder="Sua senha">
+
+
+                <label for="sobrenome">Insira novamente a senha: </label>
+                <input type="text" name="repesenha" id="repesenha" placeholder="Digite a mesma senha">
+
+
+                <input type="submit" value="Cadastrar">
+
+
+
+
+
+            </form>
+        </div>
+
+    </div>
+    
+</body>
+</html>
